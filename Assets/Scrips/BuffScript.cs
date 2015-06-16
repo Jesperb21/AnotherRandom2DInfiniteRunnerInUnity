dbc2e9d93c05df773f3d8770aa70a748609d72f8
Jesperb21 <Jesper.j.b.baunsgaard@gmail.com>
2015-06-16 13:33:29 +0200

    added buffs-ish (attempting to merge)

---
+++ Assets/Scrips/BuffScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BuffScript : MonoBehaviour {
+    private GameObject _player;
+    private GameObject _score;
+    private ScoreController _scoreController;
+    private PlayerController _playerController;
+    
+    void Start(){
+        _player = GameObject.Find("Player");
+        _score = GameObject.Find("Main Camera");
+        _playerController = (PlayerController)_player.GetComponent("PlayerController");
+        _scoreController = (ScoreController)_score.GetComponent("ScoreController");
+    }
+
+
+
+}
