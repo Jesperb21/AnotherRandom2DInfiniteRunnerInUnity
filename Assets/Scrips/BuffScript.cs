95b0dcb878d7e87581b71d66a0e0bcd7e07e2e0a
Jesperb21 <Jesper.j.b.baunsgaard@gmail.com>
2015-06-16 14:39:13 +0200

    added buff that gives points and makes a small particle explosion

--- Assets/Scrips/BuffScript.cs
+++ Assets/Scrips/BuffScript.cs
@@ -6,12 +6,28 @@ public class BuffScript : MonoBehaviour {
     private GameObject _score;
     private ScoreController _scoreController;
     private PlayerController _playerController;
+    public GameObject explosion;
     
     void Start(){
         _player = GameObject.Find("Player");
         _score = GameObject.Find("Main Camera");
         _playerController = (PlayerController)_player.GetComponent("PlayerController");
         _scoreController = (ScoreController)_score.GetComponent("ScoreController");
+    }
+    void OnCollisionEnter2D(Collision2D coll) 
+    {
+        
+        if (coll.collider.tag == "Player")
+        {
+            Instantiate(explosion, transform.position, Quaternion.identity);
+            _scoreController.score += 1000;
+            Destroy(explosion, 3);
+        }/*
+        else
+        {
+            _scoreController.score += 100000;
+        }*/
+        Destroy(gameObject);
     }
 
 
