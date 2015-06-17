3a3fbb82c61f119d931e5e6360ccc1d9a7728c8c
Jesperb21 <Jesper.j.b.baunsgaard@gmail.com>
2015-06-17 09:13:04 +0200

    added death for blocks after a while

---
+++ Assets/Scrips/destroymeeee.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class destroymeeee : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Destroy(gameObject, 10);
+	}
+}
